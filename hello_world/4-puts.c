e2f0eb83dfa4c7398849c5a4663737fdba2fb6c2
MedAliKrifa <daliolimona@gmail.com>
2022-06-07 13:04:30 +0100

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-/**hefjslkjfsjfsllo*/
 /**
- *Use the function puts
- *You are not allowed to use printf
- *Your program should end with the value 0
+ * main - execute file
+ * Return: 0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 getchar();
-return 0;
+return (0);
 }
