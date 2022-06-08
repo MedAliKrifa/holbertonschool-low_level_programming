61facf0c93c3444550707c6cc69633fc769c16b0
MedAliKrifa <daliolimona@gmail.com>
2022-06-08 22:48:04 +0100

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+for (int x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
