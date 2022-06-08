9339ac7ed6a173d23a5225b5fbe884852149ffed
MedAliKrifa <daliolimona@gmail.com>
2022-06-08 23:35:19 +0100

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - all digit of power 10
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
