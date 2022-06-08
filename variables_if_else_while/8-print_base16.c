df27c208adfd7852df140f5c62f54b6282bab9ce
MedAliKrifa <daliolimona@gmail.com>
2022-06-08 23:55:51 +0100

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print alphabet in lowercase reversed
+ * Return: 0
+ */
+int main(void)
+{
+int a = 252;
+printf("%x", a);
+printf("\n");
+return (0);
+}
