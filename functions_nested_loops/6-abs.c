322dff9310dd327331b04a29fce68285b94bc8ac
MedAliKrifa <daliolimona@gmail.com>
2022-06-09 13:17:09 +0100

    6-abs.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,13 +2,13 @@
 /**
  * print_sign - print_sign if positive or negative or 0
  * Return: On success 1.
+ * @n: integer
  */
 int _abs(int n)
 {
-
 if (n < 0)
 {
-n = (-1) * int;
+n = (-1) * n;
 _putchar('n');
 }
 else
