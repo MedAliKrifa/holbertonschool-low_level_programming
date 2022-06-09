1372a77a46cedb75e9180deb68cb3a2bdf2b42a4
MedAliKrifa <daliolimona@gmail.com>
2022-06-09 15:17:08 +0100

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - print_last_digit
+ * Return: On success 1.
+ * @n: integer
+ */
+int add(int n, int z)
+{
+int sum = n + z;
+return (sum);
+}
