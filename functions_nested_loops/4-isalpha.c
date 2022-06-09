b30851bc4e27860a336736b4cffd4f973b1761e9
MedAliKrifa <daliolimona@gmail.com>
2022-06-09 12:37:13 +0100

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - _isalpha to show if character is lowcase or uppercase
+ * Return: On success 1.
+ * @c: integer
+ */
+int _isalpha(int c)
+{
+if (c < 97 || c < 65)
+{
+return (0);
+}
+else
+{
+return (1);
+}
+}
