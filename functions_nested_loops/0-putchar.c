e78de362c35b131f41521fec80107008b9289b67
MedAliKrifa <daliolimona@gmail.com>
2022-06-09 10:20:00 +0100

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print _putchar
+ * Return: 0
+ */
+int main(void)
+{
+char x = '_';
+char a = 'p';
+char z = 'u';
+char y = 't';
+char o = 'c';
+char i = 'h';
+char l = 'a';
+char m = 'r';
+putchar(x);
+putchar(a);
+putchar(z);
+putchar(y);
+putchar(o);
+putchar(i);
+putchar(l);
+putchar(m);
+return (0);
+}
