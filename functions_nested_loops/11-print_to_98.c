0e8d87032782645e3256d265bb8180681d8f5308
MedAliKrifa <daliolimona@gmail.com>
2022-06-09 19:31:01 +0100

    11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * add - sum of two int
+ * Return: On success 1.
+ * @n: integer
+ * @z: integer
+ */
+void print_to_98(int n)
+{
+for (n = 0; n <= 98; n++)
+{
+printf("%d, ", n);
+}
+}
