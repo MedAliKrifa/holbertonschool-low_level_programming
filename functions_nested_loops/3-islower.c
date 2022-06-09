be8d6e804a6e889266c954e3ab3086d813cef9ff
MedAliKrifa <daliolimona@gmail.com>
2022-06-09 11:38:23 +0100

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet in lowercase
+ */
+int print_alphabet(main)
+{
+char x;
+if (x >= 'a' && x <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
