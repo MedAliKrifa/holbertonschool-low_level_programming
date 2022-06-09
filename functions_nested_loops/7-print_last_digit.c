51d34862f67fef9d5843104cc9f2a86c07e42913
MedAliKrifa <daliolimona@gmail.com>
2022-06-09 14:05:12 +0100

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _print_last_digit - _print_last_digit
+ * print_last_digit - print_last_digit
  * Return: On success 1.
  * @n: integer
  */
-int _print_last_digit(int n)
+int print_last_digit(int n)
 {
 _putchar(0 + n % 10);
 return (n % 10);
