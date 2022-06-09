fdd363b47de14b87fa9bdd38df591fc6fe29c8da
MedAliKrifa <daliolimona@gmail.com>
2022-06-09 19:53:26 +0100

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - all possible combinitions of digits
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
