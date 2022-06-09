f764a1a6bb07334e343ac2d8e0f864b1790a8b33
MedAliKrifa <daliolimona@gmail.com>
2022-06-09 13:07:00 +0100

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_sign - print_sign if positive or negative or 0
+ * Return: On success 1.
+ * @n: integer
+ */
+int _abs(int)
+{
+int n;
+if (n < 0)
+{
+n = (-1) * n;
+_putchar('n');
+}
+else
+{
+_putchar('n')
+}
+return (0);
+}
