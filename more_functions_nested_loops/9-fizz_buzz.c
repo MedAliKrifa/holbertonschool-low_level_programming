4b25dfab759765f60e622c72048c60dd4d3aca1e
MedAliKrifa <daliolimona@gmail.com>
2022-06-11 17:50:31 +0100

    9-fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,14 +6,19 @@
 int main(void)
 {
 int i;
-for (i = 1; i <= 100; ++i)
+for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
-printf("Fizz");
-if (i % 5 == 0)
-printf("Buzz");
-if ((i % 3 != 0) && (i % 5 != 0))
-printf("%d ", i);
+if (i % 15 == 0)
+printf("FizzBuzz\t");
+
+else if ((i % 3) == 0)
+printf("Fizz\t");
+
+else if ((i % 5) == 0)
+printf("Buzz\t");
+
+else
+printf("%d\t ", i);
 }
 
 return 0;
