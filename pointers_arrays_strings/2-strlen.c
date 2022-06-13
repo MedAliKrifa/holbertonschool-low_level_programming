78bd5aee183f341111ea33ef8192bff60b29d0cf
MedAliKrifa <daliolimona@gmail.com>
2022-06-13 21:40:56 +0100

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - _strlen to calculate length of a string
+ * Return: 0.
+ * @a: integer
+ * @b: integer
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+return (i);
+}
