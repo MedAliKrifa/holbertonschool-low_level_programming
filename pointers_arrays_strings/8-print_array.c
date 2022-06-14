b2049769f9092850d058cb4191b804dfaf5f32a6
MedAliKrifa <daliolimona@gmail.com>
2022-06-14 14:54:39 +0100

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,16 +8,16 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
+	{
+		if (i == n - 1)
 {
-if (i == n)
-{
-printf("%d", a[i]);
+	printf("%d", a[i]);
 }
-else
+	else
 {
-printf("%d,", a[i]);
-printf(" ");
+	printf("%d,", a[i]);
+	printf(" ");
 }
 }
 }
