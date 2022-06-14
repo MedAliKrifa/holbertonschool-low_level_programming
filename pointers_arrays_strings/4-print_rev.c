1353adc3ef8a163789b371fe8912112b8977d8a2
MedAliKrifa <daliolimona@gmail.com>
2022-06-14 10:02:02 +0100

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+int _strlen(char *s);
 /**
  * print_rev - print_rev to print a quote in reverse
  * Return: 0.
@@ -7,13 +7,25 @@
  */
 void print_rev(char *s)
 {
-int i, length, x;
-length = strlen(s);
-for (i = 0; i < length / 2; i++)
-{
-x = s[i];
-s[i] = s[length - i - 1];
-s[length - i - 1] = x;
+    int i, length;
+    length = _strlen(s);
+    for (i = length - 1; i >= 0; i--)
+        _putchar(s[i]);
+    _putchar('\n');
 }
-printf("%s", s);
+
+/**
+ * _strlen - _strlen to calculate length of a string
+ * Return: 0.
+ * @s: pointer
+ */
+int _strlen(char *s)
+{
+    int i, j = 0;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        j++;
+    }
+    return (i);
 }
