0a4e3dbd85e63e0ea701d38f7da284e163556066
MedAliKrifa <daliolimona@gmail.com>
2022-06-14 13:05:09 +0100

    7-puts_half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,23 +7,23 @@ int _strlen(char *s);
  */
 void puts_half(char *str)
 {
-int i, length, n;
-length = _strlen(str);
-n = (length) / 2;
+	int i, length, n;
+	length = _strlen(str);
+	n = (length) / 2;
 if (length % 2 == 0)
 {
-    for (i = length / 2; i < length; i++)
-        {
-        _putchar(str[i]);
-        }
-    }
-    else
-    {
-    for (i = n +1 ; i < length; i++)
-        {
-        _putchar(str[i]);
-        }
-    }
+for (i = length / 2; i < length; i++)
+{
+_putchar(str[i]);
+}
+}
+else
+{
+for (i = n + 1; i < length; i++)
+{
+_putchar(str[i]);
+}
+}
 _putchar('\n');
 }
 /**
