c8235b7d95df600ea2022a44e11645ae15875e07
MedAliKrifa <daliolimona@gmail.com>
2022-06-14 11:26:00 +0100

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "main.h"
+int _strlen(char *s);
+/**
+ * puts_half - puts_half to print half string
+ * Return: 0.
+ * @str: pointer
+ */
+void puts_half(char *str)
+{
+int i, length;
+length = _strlen(str);
+if (length % 2 == 0)
+{
+for (i = length / 2; i < length; i++)
+{
+_putchar(str[i]);
+}
+}
+else
+{
+for (i = length / 2 - 1; i < length; i++)
+{
+_putchar(str[i]);
+}
+}
+}
+/**
+ * _strlen - _strlen to calculate length of a string
+ * Return: 0.
+ * @s: pointer
+ */
+int _strlen(char *s)
+{
+int i, j = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+j++;
+}
+return (i);
+}
