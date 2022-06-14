cc9836ae61d805145ca0ca9007071411a502f556
MedAliKrifa <daliolimona@gmail.com>
2022-06-14 13:03:16 +0100

    7-puts_half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,18 +12,18 @@ length = _strlen(str);
 n = (length) / 2;
 if (length % 2 == 0)
 {
-for (i = length / 2; i < length; i++)
-{
-_putchar(str[i]);
-}
-}
-else
-{
-for (i = n; i < length; i++)
-{
-_putchar(str[i]);
-}
-}
+    for (i = length / 2; i < length; i++)
+        {
+        _putchar(str[i]);
+        }
+    }
+    else
+    {
+    for (i = n +1 ; i < length; i++)
+        {
+        _putchar(str[i]);
+        }
+    }
 _putchar('\n');
 }
 /**
