c9658db135fa80f93ca15203fce7d2ae32b068de
MedAliKrifa <daliolimona@gmail.com>
2022-06-15 09:37:43 +0100

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+int _strlen(char *s);
+/**
+ * rev_string - rev_string to print a string in reverse
+ * Return: 0.
+ * @s: pointer
+ */
+void rev_string(char *s)
+{
+int i, length;
+length = _strlen(s);
+for (i = length - 1; i >= 0; i--)
+putchar(s[i]);
+putchar('\n');
+}
+
+/**
+ * _strlen - _strlen to calculate length of a string
+ * Return: 0.
+ * @s: pointer
+ */
+int _strlen(char *s)
+{
+int i, j = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+j++;
+}
+return (i);
+}
