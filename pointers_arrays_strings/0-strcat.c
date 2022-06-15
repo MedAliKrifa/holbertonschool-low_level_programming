ccc9c2900af9fc0a005fd73d05a397f1cb6d19d2
MedAliKrifa <daliolimona@gmail.com>
2022-06-15 14:40:56 +0100

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int _strlen(char *s);
 /**
  * _strcat - _strcat to show if character is uppercase
  * Return: 0.
@@ -7,15 +8,29 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int i, j;
-for (i = 0; src[i] != '\0'; i++)
+int i;
+int length = _strlen(dest);
+for (i = 0; src[i] != 0; i++)
 {
+dest[length] = src[i];
+length++;
 }
-for (j = 0; dest[j] != '\0'; j++)
+dest[length] = '\0';
+return (dest);
+}
+
+/**
+ * _strlen - _strlen to calculate length of a string
+ * Return: 0.
+ * @s: pointer
+ */
+int _strlen(char *s)
 {
-src[i] = dest[j];
+int i, j = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+j++;
 }
-src[i] = '\0';
-printf("%s", src);
-return (0);
+return (i);
 }
