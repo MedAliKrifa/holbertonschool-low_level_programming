63713d785aa268fa0b9a555b2236e2107e7706a0
MedAliKrifa <daliolimona@gmail.com>
2022-06-15 20:38:48 +0100

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,18 +7,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i, a1, a2;
+int i;
 for (i = 0;; i++)
 {
 if (s1[i] == '\0' && s2[i] == '\0')
 {
 return (0);
 }
-a1 = s1[i];
-a2 = s2[i];
-if (a1 > a2)
-return (1);
-if (a2 > a1)
-return (-1);
+else
+{
+return (*s1 - *s2);
+}
 }
 }
