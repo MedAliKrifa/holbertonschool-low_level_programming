f76582bceee517626ae6a24baede5e6161b36c2f
MedAliKrifa <daliolimona@gmail.com>
2022-06-15 21:20:15 +0100

    5-string_toupper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  * string_toupper - string_toupper to reverse strings from lowcase to uppercase
+ * return: value of s
  * @s: pointer
  */
 char *string_toupper(char *s)
 {
-int i;
+int i = 0;
 while (s[i])
 {
 if (s[i] >= 'a' && s[i] <= 'z')
