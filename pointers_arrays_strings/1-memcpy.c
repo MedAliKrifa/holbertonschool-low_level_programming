4ce111dd49a2d6c3a46856b510d3c40fad80426d
MedAliKrifa <daliolimona@gmail.com>
2022-06-16 11:19:30 +0100

    1-memcpy.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - _memcpy to copy a memory area 
+ * Return: value of s.
+ * @dest: pointer
+ * @src: pointer
+ * @n: integer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
