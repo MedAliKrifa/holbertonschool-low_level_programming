1cc403aa6d140bc7f5fbddf68ea573871d411e3b
MedAliKrifa <daliolimona@gmail.com>
2022-06-16 11:44:15 +0100

    2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - _strchr to copy a memory area
+ * Return: value of s.
+ * @s: pointer
+ * @c: char
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+return (0);
+}
