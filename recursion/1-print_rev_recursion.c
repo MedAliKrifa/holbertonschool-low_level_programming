3aa91a8428c7afa93a913db4bfd9aafe5ec63308
MedAliKrifa <daliolimona@gmail.com>
2022-06-20 13:59:04 +0100

    1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - _print_rev_recursion to print string in reverse
+ * Return: value of s.
+ * @s: char
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s)
+   {
+       _print_rev_recursion(s+1);
+       _putchar(*s);
+   }
+}
