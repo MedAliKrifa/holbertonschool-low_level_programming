48bd7011c23fd59e1582e000b1f615f69334358a
MedAliKrifa <daliolimona@gmail.com>
2022-06-20 13:41:16 +0100

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,11 +6,10 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s)
-    {
-        s = s + 1;
-        _putchar(*s);
-        _putchar('\n');
-        
-    }
+if (*s)
+{
+s = s + 1;
+_putchar(*s);
+_putchar('\n');
+}
 }
