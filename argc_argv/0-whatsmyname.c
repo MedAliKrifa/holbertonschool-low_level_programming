cb494d7f2d25c24e502a400f4c047e80d344fa24
MedAliKrifa <daliolimona@gmail.com>
2022-06-23 11:03:27 +0100

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,11 +4,11 @@
  * main - main to print its name
  * @argc: integer
  * @argv: array of pointer
- * Return: 0. 
+ * Return: 0.
  */
 int main(int argc, char *argv[])
 {
     (void)argc;
-    printf("%s \n", argv[0]);
+    printf("%s\n", argv[0]);
     return (0);
 }
