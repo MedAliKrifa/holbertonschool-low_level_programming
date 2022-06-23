159eddb73f9ce108858b6cf52c14d705c3bd53c7
MedAliKrifa <daliolimona@gmail.com>
2022-06-23 11:28:41 +0100

    3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main to print the arguments of the function
+ * @argc: integer
+ * @argv: array of pointer
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+    int i, n, result;
+
+    i = 0;
+    n = 0;
+    if (argc == 3)
+    {
+        i = atoi(argv[1]);
+        n = atoi(argv[2]);
+        result = i * n;
+        printf("%d\n", result);
+    }
+    else
+    {
+        printf("Error\n");
+        return (1);
+    }
+    return (0);
+}
