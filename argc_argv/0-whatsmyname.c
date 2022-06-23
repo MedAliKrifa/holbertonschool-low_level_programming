05d20d4241ad9ba0c3cf71f51886940be43bbe09
MedAliKrifa <daliolimona@gmail.com>
2022-06-23 10:41:21 +0100

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main to print its name
+ * @argc: integer
+ * @argv: array of pointer
+ */
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    printf("%s ", argv[0]);
+    printf("\n");
+    return (0);
+}
