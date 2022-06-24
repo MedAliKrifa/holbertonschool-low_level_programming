cb1fe14f441b1fe952a247886e1fa150ba9dd5db
MedAliKrifa <daliolimona@gmail.com>
2022-06-24 22:35:19 +0100

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,29 +1,31 @@
 #include "main.h"
 /**
- * create_array - create_array to create an arrach of char
- * @size: size of array
- * @c: char
+ * _strdup - _strdup to copy a string given as parameter
+ * @str: char
  * Return: 0
  */
 char *_strdup(char *str)
 {
-    unsigned int i, length;
-    char *dupl;
-    i = 0;
-    length = strlen(str);
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    dupl = malloc(sizeof(char) * (length));
-    if (dupl == NULL)
-    {
-        return (NULL);
-    }
-    while (i < length)
-    {
-        dupl[i] = str[i];
-        i++;
-    }
-    return (dupl);
+	unsigned int i, length;
+	char *dupl;
+
+	i = 0;
+
+	length = strlen(str);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	dupl = malloc(sizeof(char) * (length));
+	if (dupl == NULL)
+	{
+		return (NULL);
+	}
+	while (i < length)
+	{
+		dupl[i] = str[i];
+		i++;
+	}
+	return (dupl);
 }
