a506c7fa027bc93d6c3ac621a75d5cc6869f6385
MedAliKrifa <daliolimona@gmail.com>
2022-06-30 14:30:03 +0100

    4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - new_dog to create a new dog
+ * @owner: owner name of type char (pointer)
+ * @age: age of dog type float
+ * @name: name of dog of type char (pointer)
+ * Return: value of dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+	if (name == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		dog->name = malloc(sizeof(strlen(name) + 1));
+		if (dog->name == NULL)
+		{
+			free(dog);
+			return (NULL);
+		}
+		dog->name = strcpy(dog->name, name);
+	}
+	if (owner == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		dog->owner = malloc(sizeof(strlen(name) + 1));
+		if (dog->owner == NULL)
+		{
+			free(dog);
+			return (NULL);
+		}
+		dog->owner = strcpy(dog->owner, owner);
+	}
+	dog->age = age;
+	return (dog);
+}
