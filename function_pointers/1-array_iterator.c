ee0cd1e2a4c4642634c80c960fb64827456f4f22
MedAliKrifa <daliolimona@gmail.com>
2022-07-01 13:15:47 +0100

    1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,13 +1,15 @@
 #include "function_pointers.h"
 /**
- * print_name - print_name to print a name using function pointer
- * @f: function
- * @name: char
+ * array_iterator - array_iterator to print an array using function pointer
+ * @action: function
+ * @array: array of functions
+ * @size: size of array
  * Return: 0
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    unsigned int x;
-    for (x = 0; x < size; x++)
-        action(array[x]);
+	unsigned int x;
+
+	for (x = 0; x < size; x++)
+		action(array[x]);
 }
