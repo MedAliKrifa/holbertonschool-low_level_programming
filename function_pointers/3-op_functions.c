e5de3c3eaa11783c954c49332b44597c4fb21622
MedAliKrifa <daliolimona@gmail.com>
2022-07-01 14:33:08 +0100

    3rd task

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add - op_add to add two int
+ * @a: first in
+ * @b: second int
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * op_sub - op_sub to sub two int
+ * @a: first in
+ * @b: second int
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * op_mul - op_mul to multip two int
+ * @a: first in
+ * @b: second int
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * op_div - op_div to div two int
+ * @a: first in
+ * @b: second int
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+    return (a / b);
+}
+
+/**
+ * op_div - op_div to mod two int
+ * @a: first in
+ * @b: second int
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+    return (a % b);
+}
