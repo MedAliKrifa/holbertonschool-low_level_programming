06fe4afd68e0e7678c957dd30962af7047308845
MedAliKrifa <daliolimona@gmail.com>
2022-07-01 13:58:48 +0100

    2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,23 +1,23 @@
 #include "function_pointers.h"
 /**
- * array_iterator - array_iterator to print an array using function pointer
- * @action: function
+ * int_index - int_index to find an integer from an array
+ * @cmp: function
  * @array: array of functions
  * @size: size of array
  * Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int x;
+	int x;
 
-    if (array == NULL)
-        return (-1);
-    else if (cmp == NULL)
-        return (-1);
-    for (x = 0; x < size; x++)
-    {
-        if (cmp(array[x]) != 0)
-            return (x);
-    }
-    return (-1);
+	if (array == NULL)
+		return (-1);
+	else if (cmp == NULL)
+		return (-1);
+	for (x = 0; x < size; x++)
+	{
+		if (cmp(array[x]) != 0)
+			return (x);
+	}
+	return (-1);
 }
