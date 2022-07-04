46a1b134bc45d4205351c22aa70a13196e6925bb
MedAliKrifa <daliolimona@gmail.com>
2022-07-04 16:35:51 +0100

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,4 +5,14 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char *const format, ...);
+typedef struct print_type
+{
+	char *type;
+	void (*f)(va_list *);
+} print_t;
+void print_string(va_list *ap);
+void print_float(va_list *ap);
+void print_integer(va_list *ap);
+void print_char(va_list *ap);
 #endif
