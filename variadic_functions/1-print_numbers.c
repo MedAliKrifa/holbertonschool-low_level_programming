38bcece6ee569de6affd22802cc791d6286023f1
MedAliKrifa <daliolimona@gmail.com>
2022-07-04 15:11:00 +0100

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,22 +2,22 @@
 /**
  * print_numbers - print_numbers to print numbers given
  * @n: const int
- * @separtor: value given to seperate numbers
+ * @separator: value given to seperate numbers
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list ap;
-    unsigned int i;
-    int x;
+	va_list ap;
+	unsigned int i;
+	int x;
 
-    va_start(ap, n);
-    for (i = 0; i < n; i++)
-    {
-        x = va_arg(ap, int);
-        printf("%d", x);
-        if (separator != NULL && i != n - 1)
-            printf("%s", separator);
-    }
-    printf("\n");
-    va_end(ap);
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(ap, int);
+		printf("%d", x);
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
 }
