249ab22c06787d0be56a955adee92f1aa39e694c
MedAliKrifa <daliolimona@gmail.com>
2022-07-14 02:55:53 +0100

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,43 +1,41 @@
 #include "lists.h"
 /**
- * add_node - add_node to add node to the list
+ * add_node_end - add_node_end to add node to the end of list
  * @head: head node
  * @str: char
  * Return: value of i
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *i;
-    list_t *end;
+	list_t *i;
+	list_t *end;
 
-    if (str == NULL)
-    {
-        return (NULL);
-    }
-    i = malloc(sizeof(list_t));
-    if (i == NULL)
-    {
-        return (NULL);
-    }
-    i->str = strdup(str);
-    if (i->str == NULL)
-    {
-        free(i);
-        return (NULL);
-    }
-    i->len = strlen(i->str);
-    i->next = NULL;
-    if (*head == NULL)
-    {
-        *head = i;
-        return (i);
-    }
-    end = *head;
-    while (end->next)
-    {
-        end = end->next;
-        end->next = i;
-    }
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	i = malloc(sizeof(list_t));
+	if (i == NULL)
+	{
+		return (NULL);
+	}
+	i->str = strdup(str);
+	if (i->str == NULL)
+	{
+		free(i);
+		return (NULL);
+	}
+	i->len = strlen(i->str);
+	i->next = NULL;
+	if (*head == NULL)
+	{
+		*head = i;
+		return (i);
+	}
+	end = *head;
+	while (end->next)
+		end = end->next;
+	end->next = i;
 
-    return (i);
+	return (i);
 }
