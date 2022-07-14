b615fe155bc722f26aedb508237cd625fa4e928c
MedAliKrifa <daliolimona@gmail.com>
2022-07-14 03:05:06 +0100

    4-free_list.c

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -2,18 +2,16 @@
 /**
  * free_list - free_list to free a list
  * @head: head node
- * @str: char
- * Return: value of i
  */
 void free_list(list_t *head)
 {
-    list_t *i;
+	list_t *i;
 
-    while (head != NULL)
-    {
-	   i = head->next;
-	   free(head->str);
-	   free(head);
-	   head = i;
-    }
+	while (head != NULL)
+	{
+		i = head->next;
+		free(head->str);
+		free(head);
+		head = i;
+	}
 }
