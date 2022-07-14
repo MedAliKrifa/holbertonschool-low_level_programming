942746370a7e39ead705b84812e5586e04848b5b
MedAliKrifa <daliolimona@gmail.com>
2022-07-14 17:22:39 +0100

    4-free_listint.c

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 /**
- * free_list - free_list to free a list
+ * free_listint - free_listint to free a list
  * @head: head node
  */
 void free_listint(listint_t *head)
 {
-    listint_t *f;
-    while (head != NULL)
-    {
-        f = head->next;
-        free(head);
-        head = f;
-    }
+	listint_t *f;
+	while (head != NULL)
+	{
+		f = head->next;
+		free(head);
+		head = f;
+	}
 }
