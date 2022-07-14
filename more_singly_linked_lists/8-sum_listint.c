790040342907ccf87bdb07a3348f6354002577b1
MedAliKrifa <daliolimona@gmail.com>
2022-07-14 17:40:05 +0100

    8-sum_listint.c

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * sum_listint - sum_listint to print sum of 2 lists
+ * @head: head node
+ * Return: value of i
+ */
+int sum_listint(listint_t *head)
+{
+	int i = 0;
+	while (head)
+	{
+		i = i + head->n;
+		head = head->next;
+	}
+	return (i);
+}
