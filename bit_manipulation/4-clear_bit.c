c1b39990d0fe35cb1586b2d35f2fb3f27687dfdd
MedAliKrifa <daliolimona@gmail.com>
2022-07-20 01:06:25 +0100

    4-clear_bit.c

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * set_bit - set_bit to set the value of a given bit to 0
+ * clear_bit - clear_bit to set the value of a given bit to 0
  * @n: int
  * @index: int
  * Return: t1
@@ -8,8 +8,8 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (index >= (sizeof(unsigned long int) * 8))
-        return (-1);
-    *n &= ~(1 << index);
-    return (1);
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
 }
