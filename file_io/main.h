8a621571fd24ee4a0af5d11beb9ba6eebd7f8df2
MedAliKrifa <daliolimona@gmail.com>
2022-07-21 12:21:07 +0100

    main.h

---
+++ file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <math.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
