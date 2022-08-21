d801eacff50fb100c10e6f21b9ab0b43466a1891
MedAliKrifa <daliolimona@gmail.com>
2022-08-21 16:09:07 +0100

    2-key_index.c

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * key_index - function that gives index key
+ * @key: key
+ *@size: hashtable size
+ * Return: key index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+        unsigned long int index = 0;
+        unsigned long int slot = hash_djb2(key);
+        index = slot % size;
+        return (index);
+}
