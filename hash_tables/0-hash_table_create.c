26884582a68e293a5a8bf52cd1396f088dc8071b
MedAliKrifa <daliolimona@gmail.com>
2022-08-21 15:58:43 +0100

    0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,21 +6,21 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table;
-    unsigned int i;
+	hash_table_t *table;
+	unsigned int i;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    table = malloc(sizeof(hash_table_t));
-    if (table == NULL)
-        return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
 
-    table->size = size;
-    table->array = malloc(sizeof(hash_node_t *) * size);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
 
-    for (i = 0; i < size; i++)
-        table->array[i] = NULL;
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
-    return (table);
+	return (table);
 }
