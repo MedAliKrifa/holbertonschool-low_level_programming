3d4b320523a3de21b90cfa8cab7d3fd2b3d8a670
MedAliKrifa <daliolimona@gmail.com>
2022-08-21 16:09:54 +0100

    2-key_index.c

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -7,8 +7,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-        unsigned long int index = 0;
-        unsigned long int slot = hash_djb2(key);
-        index = slot % size;
-        return (index);
+		unsigned long int index = 0;
+		unsigned long int slot = hash_djb2(key);
+		index = slot % size;
+		return (index);
 }
