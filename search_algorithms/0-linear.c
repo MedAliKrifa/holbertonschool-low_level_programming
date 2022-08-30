09cf1743afc42eca1f0175b4bb6a5a3e37b31f5e
MedAliKrifa <daliolimona@gmail.com>
2022-08-31 00:54:47 +0100

    0-linear.c

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,19 @@
+#include "search_algos.h"
+/**
+ * linear_search - function to search for int in an array
+ * @array: array of int
+ * @size: size of array
+ * @value: values inside the array
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i;
+
+    for (i = 0; i < size; i++)
+    {
+        printf("Value checked array[%ld] = [%d]", i, array[i]);
+        if (array[i] == value)
+            return (i);
+    }
+    return -1;
+}
